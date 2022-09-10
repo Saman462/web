6aaf0b2535c2b68de203c230ff0306af8d2062e4
Saman462 <szohra.bese-26mcs@student.nust.edu.pk>
2022-09-10 18:35:50 +0500

    responsiveness

--- components/banners.js
+++ components/banners.js
@@ -20,15 +20,25 @@ import banner1 from "../public/assets/Asset6_1.png";
 import useSWR from "swr";
 import style from "../styles/Home.module.css";
 let theme = createTheme();
-theme = responsiveFontSizes(theme);
-const useStyles = makeStyles({
-  bannerContainer: {
-    border: "solid black 4px",
-    position: "relative",
-    width: "50%",
-    height: "700px",
+theme.typography.h3 = {
+  fontSize: "1.2rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.5rem",
   },
-
+  [theme.breakpoints.up("md")]: {
+    fontSize: "2rem",
+  },
+};
+theme.typography.body1 = {
+  fontSize: "1rem",
+  "@media (min-width:600px)": {
+    fontSize: "1.1rem",
+  },
+  [theme.breakpoints.up("md")]: {
+    fontSize: "1.5rem",
+  },
+};
+const useStyles = makeStyles({
   card1: {
     border: "none",
     boxShadow: "none",
@@ -77,7 +87,7 @@ function Banners() {
             <ThemeProvider theme={theme}>
               <Typography
                 gutterBottom
-                variant="h4"
+                variant="h3"
                 component="div"
                 fontFamily="Lato"
               >
